2df2f9762e1362378c639d3c1134f904dd43ec63
Tony Sullivan <tony@navillus.nl>
2021-07-03 13:12:23 +0200

    feat: :bug: Adding the repro showing class names leaking out

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
 export default {
   /** Where to resolve all URLs relative to. Useful if you have a monorepo project. */
-  projectRoot: '.',
+  projectRoot: ".",
   /** Path to Astro components, pages, and data */
-  astroRoot: './src',
+  astroRoot: "./src",
   /** When running `astro build`, path to final static output */
-  dist: './dist',
+  dist: "./dist",
   /** A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing. */
-  public: './public',
+  public: "./public",
   /** Extension-specific handlings */
   extensions: {
     /** Set this to "preact" or "react" to determine what *.jsx files should load */
-    '.jsx': 'react',
+    ".jsx": "react",
   },
   /** Options specific to `astro build` */
   buildOptions: {
     /** Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs. */
-    site: '',
+    /** site: '', */
     /** Generate sitemap (set to "false" to disable) */
     sitemap: true,
   },
@@ -27,10 +27,5 @@ export default {
     tailwindConfig: undefined,
   },
   /** default array of rendering packages inserted into runtime */
-  renderers: [
-    '@astro-renderer/preact',
-    '@astro-renderer/react',
-    '@astro-renderer/svelte',
-    '@astro-renderer/vue',
-  ],
+  renderers: [],
 };
